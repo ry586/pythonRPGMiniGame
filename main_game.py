cf37135240e045296b003a067d9433e053c73619
Rylan <ryfranz18@gmail.com>
2024-01-01 13:50:26 -0600

    Add menu options

--- main_game.py
+++ main_game.py
@@ -1,5 +1,9 @@
 # RPG Python Game
 
-print("Welcome to the RPG Game, Please select and option below:")
+print("Welcome to the RPG Game, Please select and option below:\n")
 
 dataIn = input("Select Choice")
+print("1) Start Game")
+print("2) Directions")
+print("3) Quit")
+
